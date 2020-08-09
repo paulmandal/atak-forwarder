e838a25d921562ff9d551e6ed81c7ac1476c631c
Paul Mandal <paul.mandal@gmail.com>
2020-08-09 11:18:24 -0600

    Listen to loopback from ATAK, should be able to break these messages up and send via GoTenna

---
+++ app/src/main/java/com/paulmandal/atak/gotenna/mesh/MainActivity.java
@@ -0,0 +1,57 @@
+package com.paulmandal.atak.gotenna.mesh;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.paulmandal.atak.gotenna.mesh.services.UdpListenerService;
+
+public class MainActivity extends AppCompatActivity {
+    UdpListenerService mService;
+    boolean mBound = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Bind to LocalService
+        Intent intent = new Intent(this, UdpListenerService.class);
+        startService(intent);
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unbindService(connection);
+        mBound = false;
+    }
+
+    /** Defines callbacks for service binding, passed to bindService() */
+    private ServiceConnection connection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className,
+                                       IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            UdpListenerService.LocalBinder binder = (UdpListenerService.LocalBinder) service;
+            mService = binder.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+            mBound = false;
+        }
+    };
+}

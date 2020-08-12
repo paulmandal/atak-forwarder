5cb8b2c054260cb2ca026f4b7cc7bb8db171eac3
Paul Mandal <paul.mandal@gmail.com>
2020-08-11 21:34:36 -0600

    Remove dead code from repo

--- app/src/main/java/com/paulmandal/atak/forwarder/plugin/ForwarderLifecycle.java
+++ app/src/main/java/com/paulmandal/atak/forwarder/plugin/ForwarderLifecycle.java
@@ -10,7 +10,6 @@ import com.paulmandal.atak.forwarder.factories.CommHardwareFactory;
 import com.paulmandal.atak.forwarder.factories.MessageHandlerFactory;
 import com.paulmandal.atak.forwarder.handlers.InboundMessageHandler;
 import com.paulmandal.atak.forwarder.handlers.OutboundMessageHandler;
-import com.paulmandal.atak.forwarder.helpers.GoTennaHelper;
 import com.paulmandal.atak.forwarder.interfaces.CommHardware;
 
 import transapps.maps.plugin.lifecycle.Lifecycle;
@@ -18,19 +17,11 @@ import transapps.maps.plugin.lifecycle.Lifecycle;
 public class ForwarderLifecycle implements Lifecycle {
     private final static String TAG = "ATAKDBG." + ForwarderLifecycle.class.getSimpleName();
 
-//    private final Context mContext;
     private Activity mActivity;
-//    private MapView mMapView;
 
     private CommHardware mCommHardware;
     private InboundMessageHandler mInboundMessageHandler;
     private OutboundMessageHandler mOutboundMessageHandler;
-
-    public ForwarderLifecycle() { //Context ctx) {
-//        this.mContext = ctx;
-//        this.mMapView = null;
-//        PluginNativeLoader.init(ctx);
-    }
 
     @Override
     public void onCreate(final Activity activity, final transapps.mapi.MapView transappsMapView) {
@@ -39,7 +30,6 @@ public class ForwarderLifecycle implements Lifecycle {
             return;
         }
         mActivity = activity;
-//        mMapView = (MapView) transappsMapView.getView();
     }
 
     @Override
@@ -51,10 +41,7 @@ public class ForwarderLifecycle implements Lifecycle {
 
     @Override
     public void onStart() {
-        Log.d(TAG, "onStart");
-        // TODO: this won't wait for activity result, however we should already have Bluetooth perms via ATAK
-        GoTennaHelper.checkPermissionsAndSetupSdk(mActivity);
-
+        Log.d(TAG, "onStart()");
         mCommHardware = CommHardwareFactory.getCommHardware();
         mInboundMessageHandler = MessageHandlerFactory.getInboundMessageHandler();
         mOutboundMessageHandler = MessageHandlerFactory.getOutboundMessageHandler();

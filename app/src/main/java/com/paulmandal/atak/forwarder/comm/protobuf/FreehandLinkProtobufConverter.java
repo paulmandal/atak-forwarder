f37816cc29ca6494b88e48af36f5e32e788d6428
Paul Mandal <paul.mandal@gmail.com>
2020-08-29 23:48:53 -0600

    Improve Freehand shape message by using gzip

--- app/src/main/java/com/paulmandal/atak/forwarder/comm/protobuf/FreehandLinkProtobufConverter.java
+++ app/src/main/java/com/paulmandal/atak/forwarder/comm/protobuf/FreehandLinkProtobufConverter.java
@@ -2,7 +2,11 @@ package com.paulmandal.atak.forwarder.comm.protobuf;
 
 import com.atakmap.coremap.cot.event.CotAttribute;
 import com.atakmap.coremap.cot.event.CotDetail;
+import com.google.protobuf.ByteString;
+import com.paulmandal.atak.forwarder.comm.protobuf.gzip.GzipHelper;
 import com.paulmandal.atak.forwarder.protobufs.ProtobufFreehandLink;
+
+import java.io.IOException;
 
 public class FreehandLinkProtobufConverter {
     private static final String KEY_LINK = "link";
@@ -15,7 +19,11 @@ public class FreehandLinkProtobufConverter {
         for (CotAttribute attribute : attributes) {
             switch (attribute.getName()) {
                 case KEY_LINE:
-                    builder.setLine(attribute.getValue()); // TODO: gzip or EXI this
+                    try {
+                        builder.setLine(ByteString.copyFrom(GzipHelper.compress(attribute.getValue()))); // TODO: evaluate this using EXI instead
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                     break;
                 default:
                     throw new UnknownDetailFieldException("Don't know how to handle detail field: freehandlink." + attribute.getName());
@@ -28,8 +36,13 @@ public class FreehandLinkProtobufConverter {
         if (freehandLink != null && freehandLink != ProtobufFreehandLink.MinimalFreehandLink.getDefaultInstance()) {
             CotDetail freehandLinkDetail = new CotDetail(KEY_LINK);
 
-            if (!StringUtils.isNullOrEmpty(freehandLink.getLine())) {
-                freehandLinkDetail.setAttribute(KEY_LINE, freehandLink.getLine());
+            ByteString freehandLine = freehandLink.getLine();
+            if (freehandLine != ProtobufFreehandLink.MinimalFreehandLink.getDefaultInstance().getLine()) {
+                try {
+                    freehandLinkDetail.setAttribute(KEY_LINE, GzipHelper.decompress(freehandLine.toByteArray()));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
 
             cotDetail.addChild(freehandLinkDetail);

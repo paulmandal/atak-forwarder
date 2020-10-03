98434e32fa1b48f026a9fda32ef3241c97518578
Paul Mandal <paul.mandal@gmail.com>
2020-10-03 05:49:29 -0600

    Use CotDispatcher to dispatch incoming CoTs

--- app/src/main/java/com/paulmandal/atak/forwarder/Config.java
+++ app/src/main/java/com/paulmandal/atak/forwarder/Config.java
@@ -38,17 +38,6 @@ public class Config {
     public static final int DEFAULT_PLI_CACHE_PURGE_TIME_MS = 20000;
 
     /**
-     * IP and port to retransmit inbound messages to, this should work with the defaults in ATAK
-     * Settings / Network Connections / Network Connections / Manage Inputs / 0.0.0.0:4242:udp
-     */
-    public static final int INBOUND_MESSAGE_DEST_PORT = 4242;
-
-    /**
-     * This can be anything > 1024 and < 65535
-     */
-    public static final int INBOUND_MESSAGE_SRC_PORT = 17233;
-
-    /**
      * TAG prefix for all debug messages
      */
     public static final String DEBUG_TAG_PREFIX = "ATAKDBG.";

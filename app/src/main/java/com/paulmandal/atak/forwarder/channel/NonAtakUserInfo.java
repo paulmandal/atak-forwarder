487a004c515345b0d8aa633303b7bd33c2f702bf
a-f-G-U-C <65810997+a-f-G-U-C@users.noreply.github.com>
2020-11-18 13:09:26 +0000

    lazy fix issue #72
    lazy fix issue #72

--- app/src/main/java/com/paulmandal/atak/forwarder/channel/NonAtakUserInfo.java
+++ app/src/main/java/com/paulmandal/atak/forwarder/channel/NonAtakUserInfo.java
@@ -6,14 +6,16 @@ public class NonAtakUserInfo extends UserInfo {
     public double lat;
     public double lon;
     public int altitude;
+    public boolean gpsValid;
     public String shortName;
 
-    public NonAtakUserInfo(String callsign, String meshId, @Nullable Integer batteryPercentage, double lat, double lon, int altitude, String shortName) {
+    public NonAtakUserInfo(String callsign, String meshId, @Nullable Integer batteryPercentage, double lat, double lon, int altitude, boolean gpsValid, String shortName) {
         super(callsign, meshId, null, batteryPercentage);
 
         this.lat = lat;
         this.lon = lon;
         this.altitude = altitude;
+        this.gpsValid = gpsValid;
         this.shortName = shortName;
     }
 }

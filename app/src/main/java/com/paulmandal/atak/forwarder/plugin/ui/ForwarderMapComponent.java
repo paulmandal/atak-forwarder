61267cff083e2a5a7117fc1dab8c1f5e64bad693
Paul Mandal <paul.mandal@gmail.com>
2021-02-06 20:55:22 -0700

    Fix an incorrect ctor signature

--- app/src/main/java/com/paulmandal/atak/forwarder/plugin/ui/ForwarderMapComponent.java
+++ app/src/main/java/com/paulmandal/atak/forwarder/plugin/ui/ForwarderMapComponent.java
@@ -77,7 +77,7 @@ public class ForwarderMapComponent extends DropDownMapComponent {
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
-        TrackerCotGenerator trackerCotGenerator = new TrackerCotGenerator(mDestroyables, userTracker, inboundMessageHandler, pluginVersion, mapView.getDeviceCallsign());
+        TrackerCotGenerator trackerCotGenerator = new TrackerCotGenerator(mDestroyables, userTracker, inboundMessageHandler, pluginVersion);
 
         HashHelper hashHelper = new HashHelper();
         StatusViewModel statusViewModel = new StatusViewModel(mDestroyables, sharedPreferences, userTracker, meshtasticCommHardware, commandQueue, hashHelper);

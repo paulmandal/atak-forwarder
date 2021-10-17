3f7f60a33444c243d0a8f23364e637d4cc61ac55
Paul Mandal <paul.mandal@gmail.com>
2021-10-16 19:52:07 -0600

    Update release pkg script

--- mk-release-pkg.sh
+++ mk-release-pkg.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
 if [ "${1}" == "" ]
 then
-  echo "Usage: ${0} <version>"
+  echo "Usage: ${0} <version> [target ATAK version]"
   exit 1
 fi
 
+if [ "${2}" != "" ]
+then
+  currentAtakVersion=`grep ext.ATAK_VERSION = \".*\" | sed "s/.*ext.ATAK_VERSION = \"//" | sed "s/\"//"`
+  echo "current atak version: $currentAtakVersion"
+  sed -i "s/ext.ATAK_VERSION = \".*\"/ext.ATAK_VERSION = \"${2}\"/" app/build.gradle
+fi
+
 mv -v local.properties ..
-zip -r /mnt/shared/atak-forwarder-${version}-release.zip . --exclude build app/build images
+if [ "${2}" != "" ]
+then
+  zip -r /mnt/shared/atak-forwarder-${1}-atak-${2}.zip . --exclude build app/build images
+else
+  zip -r /mnt/shared/atak-forwarder-${1}.zip . --exclude build app/build images
+fi
+
 cp -v ../local.properties .
+
+if [ "${2}" != "" ]
+then
+  sed -i "s/ext.ATAK_VERSION = \"${2}\"/ext.ATAK_VERSION = \"${currentAtakVersion}\"/" app/build.gradle
+fi
 

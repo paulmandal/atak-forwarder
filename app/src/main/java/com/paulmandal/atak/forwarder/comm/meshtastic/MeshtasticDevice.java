2198b1fb130ef041d60e7e150902ff73486a02d4
Paul Mandal <paul.mandal@gmail.com>
2021-10-24 16:09:59 -0600

    Fix Gson+ProGuard handling

--- app/src/main/java/com/paulmandal/atak/forwarder/comm/meshtastic/MeshtasticDevice.java
+++ app/src/main/java/com/paulmandal/atak/forwarder/comm/meshtastic/MeshtasticDevice.java
@@ -2,14 +2,19 @@ package com.paulmandal.atak.forwarder.comm.meshtastic;
 
 import androidx.annotation.NonNull;
 
+import com.google.gson.annotations.SerializedName;
+
 public class MeshtasticDevice {
     public enum DeviceType {
         BLUETOOTH,
         USB
     }
 
+    @SerializedName("name")
     public final String name;
+    @SerializedName("address")
     public final String address;
+    @SerializedName("deviceType")
     public final DeviceType deviceType;
 
     public MeshtasticDevice(String name, String address, DeviceType deviceType) {

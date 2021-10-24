9da38612435c015a0249f76af577e7b25264e169
Paul Mandal <paul.mandal@gmail.com>
2021-10-24 12:24:32 -0600

    Re-add GSON proguard config stuff since it was not the cause of our pipeline failures

--- app/src/main/java/com/paulmandal/atak/forwarder/gson/ChannelConfig.java
+++ app/src/main/java/com/paulmandal/atak/forwarder/gson/ChannelConfig.java
@@ -1,9 +1,15 @@
 package com.paulmandal.atak.forwarder.gson;
 
+import com.google.gson.annotations.SerializedName;
+
 public class ChannelConfig implements Cloneable {
+    @SerializedName("name")
     public String name;
+    @SerializedName("psk")
     public byte[] psk;
+    @SerializedName("modemConfig")
     public int modemConfig;
+    @SerializedName("isDefault")
     public boolean isDefault;
 
     public ChannelConfig(String name, byte[] psk, int modemConfig, boolean isDefault) {

03e1fc3b92d712d96b0a26f7bdfff4298bea7222
Paul Mandal <paul.mandal@gmail.com>
2022-01-30 16:24:11 -0700

    Make mk-release-pkg.sh a little nicer

--- mk-release-pkg.sh
+++ mk-release-pkg.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
-if [ "${1}" == "" ]
-then
-  echo "Usage: ${0} <version> [target ATAK version]"
-  exit 1
-fi
+pluginVersion=`grep "ext.PLUGIN_VERSION = \".*\"" app/build.gradle | sed "s/.*ext.PLUGIN_VERSION = \"//" | sed "s/\"//"`
+targetAtakVersion="${1}"
 
-if [ "${2}" != "" ]
+if [ "${targetAtakVersion}" != "" ]
 then
   currentAtakVersion=`grep "ext.ATAK_VERSION = \".*\"" app/build.gradle | sed "s/.*ext.ATAK_VERSION = \"//" | sed "s/\"//"`
-  echo "current atak version: $currentAtakVersion"
-  sed -i "s/ext.ATAK_VERSION = \".*\"/ext.ATAK_VERSION = \"${2}\"/" app/build.gradle
+  sed -i "s/ext.ATAK_VERSION = \".*\"/ext.ATAK_VERSION = \"${targetAtakVersion}\"/" app/build.gradle
 fi
 
 repoDirName=`pwd | sed s#.*/##`
 
 pushd ..
-if [ "${2}" != "" ]
+if [ "${targetAtakVersion}" != "" ]
 then
-  zip -qr /mnt/shared/atak-forwarder-${1}-atak-${2}.zip "${repoDirName}" --exclude "*/build/*" "${repoDirName}/images/*" "${repoDirName}/.git/*" "${repoDirName}/local.properties" "${repoDirName}/.idea/*"
+  zip -qr /mnt/shared/atak-forwarder-${pluginVersion}-atak-${targetAtakVersion}.zip "${repoDirName}" --exclude "*/build/*" "${repoDirName}/images/*" "${repoDirName}/.git/*" "${repoDirName}/local.properties" "${repoDirName}/.idea/*"
 else
-  zip -qr /mnt/shared/atak-forwarder-${1}.zip "${repoDirName}" --exclude "*/build/*" "${repoDirName}/images/*" "${repoDirName}/.git/*" "${repoDirName}/local.properties" "${repoDirName}/.idea/*"
+  zip -qr /mnt/shared/atak-forwarder-${pluginVersion}.zip "${repoDirName}" --exclude "*/build/*" "${repoDirName}/images/*" "${repoDirName}/.git/*" "${repoDirName}/local.properties" "${repoDirName}/.idea/*"
 fi
 popd
 
-if [ "${2}" != "" ]
+if [ "${targetAtakVersion}" != "" ]
 then
-  sed -i "s/ext.ATAK_VERSION = \"${2}\"/ext.ATAK_VERSION = \"${currentAtakVersion}\"/" app/build.gradle
-  echo "wrote: /mnt/shared/atak-forwarder-${1}-atak-${2}.zip"
+  sed -i "s/ext.ATAK_VERSION = \"${targetAtakVersion}\"/ext.ATAK_VERSION = \"${currentAtakVersion}\"/" app/build.gradle
+  echo "wrote: /mnt/shared/atak-forwarder-${pluginVersion}-atak-${targetAtakVersion}.zip"
 else
-  echo "wrote: /mnt/shared/atak-forwarder-${1}.zip"
+  echo "wrote: /mnt/shared/atak-forwarder-${pluginVersion}.zip"
 fi
 

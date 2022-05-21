21053b1ff3c7790cf97c70f5d3fae58412e79d60
Paul Mandal <paul.mandal@gmail.com>
2022-05-21 10:23:15 -0600

    Converted to ListView since RV doesn't work in ATAK (maybe?)

--- app/src/main/java/com/paulmandal/atak/forwarder/plugin/ui/LoggingScreen.java
+++ app/src/main/java/com/paulmandal/atak/forwarder/plugin/ui/LoggingScreen.java
@@ -2,18 +2,16 @@ package com.paulmandal.atak.forwarder.plugin.ui;
 
 import android.content.Context;
 import android.util.AttributeSet;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.lifecycle.LifecycleOwner;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.paulmandal.atak.forwarder.R;
 import com.paulmandal.atak.forwarder.plugin.ui.viewmodels.LoggingViewModel;
-import com.paulmandal.atak.forwarder.plugin.ui.viewmodels.StatusViewModel;
 
 public class LoggingScreen extends ConstraintLayout {
-    private final RecyclerView mLoggingRecyclerView;
+    private final ListView mLoggingListView;
 
     public LoggingScreen(Context context) {
         this(context, null);
@@ -27,15 +25,15 @@ public class LoggingScreen extends ConstraintLayout {
         super(context, attrs, defStyleAttr);
         inflate(context, R.layout.logging_layout, this);
 
-        mLoggingRecyclerView = findViewById(R.id.logging_recyclerview);
+        mLoggingListView = findViewById(R.id.listview_logging_messages);
     }
 
     public void bind(LifecycleOwner lifecycleOwner,
                      Context pluginContext,
                      LoggingViewModel loggingViewModel) {
         loggingViewModel.getLogMessages().observe(lifecycleOwner, logMessages -> {
-            LogMessageAdapter logMessageAdapter = new LogMessageAdapter(pluginContext, logMessages);
-            mLoggingRecyclerView.setAdapter(logMessageAdapter);
+            LogMessageDataAdapter logMessageAdapter = new LogMessageDataAdapter(pluginContext, logMessages);
+            mLoggingListView.setAdapter(logMessageAdapter);
         });
     }
 }

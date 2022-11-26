8aba58f663cb0fa63b2e1f63bda89da099a3f634
Paul Mandal <paul.mandal@gmail.com>
2022-11-26 00:06:47 -0700

    Work on MeshDeviceConfigurator a bit

--- app/src/main/java/com/paulmandal/atak/forwarder/comm/meshtastic/RMeshDeviceConfigurator.java
+++ app/src/main/java/com/paulmandal/atak/forwarder/comm/meshtastic/RMeshDeviceConfigurator.java
@@ -1,5 +1,8 @@
 package com.paulmandal.atak.forwarder.comm.meshtastic;
 
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
 public class RMeshDeviceConfigurator implements RMeshConnectionHandler.DeviceConnectionStateListener {
     public enum ConfigurationState {
         CHECKING,
@@ -11,14 +14,46 @@ public class RMeshDeviceConfigurator implements RMeshConnectionHandler.DeviceCon
         void onConfigurationStateChanged(ConfigurationState configurationState);
     }
 
-    public RMeshDeviceConfigurator(RMeshConnectionHandler meshConnectionHandler) {
+    private final RMeshServiceController mMeshServiceController;
+    private final Set<ConfigurationStateListener> mListeners = new CopyOnWriteArraySet<>();
+
+    private boolean mConfigurationChecked;
+    private boolean mConfigurationWritten;
+
+    public RMeshDeviceConfigurator(RMeshConnectionHandler meshConnectionHandler,
+                                   RMeshServiceController meshServiceController) {
+        mMeshServiceController = meshServiceController;
+
         meshConnectionHandler.addListener(this);
     }
 
     @Override
     public void onDeviceConnectionStateChanged(RMeshConnectionHandler.DeviceConnectionState deviceConnectionState) {
-        // TODO: check config
-        // TODO: build and write config if necessary
-        // TODO: validate config on reconnect
+        if (deviceConnectionState == RMeshConnectionHandler.DeviceConnectionState.CONNECTED) {
+            if (!mConfigurationChecked) {
+                notifyListeners(ConfigurationState.CHECKING);
+
+                if (maybeWriteConfig()) {
+                    return;
+                }
+            }
+
+            if (mConfigurationWritten) {
+                notifyListeners(ConfigurationState.FINISHED);
+            }
+        }
+    }
+
+    private boolean maybeWriteConfig() {
+        mMeshServiceController.getMeshService().getConfig();
+        // TODO: compare config
+        // TODO: write config
+        // TODO: notify listeners
+    }
+
+    private void notifyListeners(ConfigurationState configurationState) {
+        for (ConfigurationStateListener listener : mListeners) {
+            listener.onConfigurationStateChanged(configurationState);
+        }
     }
 }

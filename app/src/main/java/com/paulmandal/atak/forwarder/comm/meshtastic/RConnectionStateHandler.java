5dc365536e5158725dc7f610331419a7a3d529e3
Paul Mandal <paul.mandal@gmail.com>
2022-11-26 17:16:41 -0700

    Finish ConnectionStateHandler

--- app/src/main/java/com/paulmandal/atak/forwarder/comm/meshtastic/RConnectionStateHandler.java
+++ app/src/main/java/com/paulmandal/atak/forwarder/comm/meshtastic/RConnectionStateHandler.java
@@ -1,6 +1,13 @@
 package com.paulmandal.atak.forwarder.comm.meshtastic;
 
-public class RConnectionStateHandler {
+import androidx.annotation.Nullable;
+
+import com.geeksville.mesh.ConfigProtos;
+
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+public class RConnectionStateHandler implements RMeshDeviceConfigurationController.Listener, RMeshServiceController.ServiceConnectionStateListener, RMeshConnectionHandler.DeviceConnectionStateListener, RDeviceConfigObserver.DeviceConfigListener {
     public enum ConnectionState {
         NO_DEVICE_CONFIGURED,
         NO_SERVICE_CONNECTION,
@@ -13,4 +20,83 @@ public class RConnectionStateHandler {
         void onConnectionStateChanged(ConnectionState connectionState);
     }
 
+    private final Set<ConnectionStateListener> mListeners = new CopyOnWriteArraySet<>();
+
+    private RMeshConnectionHandler.DeviceConnectionState mDeviceConnectionState;
+    private RMeshDeviceConfigurationController.ConfigurationState mDeviceConfigurationState;
+    private RMeshServiceController.ServiceConnectionState mServiceConnectionState;
+    private MeshtasticDevice mMeshtasticDevice;
+
+    public RConnectionStateHandler(@Nullable MeshtasticDevice meshtasticDevice,
+                                   RMeshDeviceConfigurationController meshDeviceConfigurationController,
+                                   RMeshServiceController meshServiceController,
+                                   RMeshConnectionHandler meshConnectionHandler,
+                                   RDeviceConfigObserver deviceConfigObserver) {
+        mMeshtasticDevice = meshtasticDevice;
+
+        meshDeviceConfigurationController.addListener(this);
+        meshServiceController.addConnectionStateListener(this);
+        meshConnectionHandler.addListener(this);
+        deviceConfigObserver.addListener(this);
+    }
+
+    @Override
+    public void onDeviceConnectionStateChanged(RMeshConnectionHandler.DeviceConnectionState deviceConnectionState) {
+        mDeviceConnectionState = deviceConnectionState;
+        notifyListeners();
+    }
+
+    @Override
+    public void onConfigurationStateChanged(RMeshDeviceConfigurationController.ConfigurationState configurationState) {
+        mDeviceConfigurationState = configurationState;
+        notifyListeners();
+    }
+
+    @Override
+    public void onServiceConnectionStateChanged(RMeshServiceController.ServiceConnectionState serviceConnectionState) {
+        mServiceConnectionState = serviceConnectionState;
+        notifyListeners();
+    }
+
+    @Override
+    public void onSelectedDeviceChanged(MeshtasticDevice meshtasticDevice) {
+        mMeshtasticDevice = meshtasticDevice;
+        notifyListeners();
+    }
+
+    @Override
+    public void onDeviceConfigChanged(ConfigProtos.Config.LoRaConfig.RegionCode regionCode, String channelName, int channelMode, byte[] channelPsk, ConfigProtos.Config.DeviceConfig.Role deviceRole) {
+        // do nothing
+    }
+
+    private ConnectionState determineConnectionState() {
+        if (mMeshtasticDevice == null) {
+            return ConnectionState.NO_DEVICE_CONFIGURED;
+        }
+
+        if (mServiceConnectionState == RMeshServiceController.ServiceConnectionState.DISCONNECTED) {
+            return ConnectionState.NO_SERVICE_CONNECTION;
+        }
+
+        if (mDeviceConfigurationState != RMeshDeviceConfigurationController.ConfigurationState.READY) {
+            return ConnectionState.DEVICE_UNCONFIGURED;
+        }
+
+        if (mDeviceConnectionState == RMeshConnectionHandler.DeviceConnectionState.DISCONNECTED) {
+            return ConnectionState.DEVICE_DISCONNECTED;
+        }
+
+        if (mDeviceConnectionState == RMeshConnectionHandler.DeviceConnectionState.CONNECTED) {
+            return ConnectionState.DEVICE_CONNECTED;
+        }
+
+        return ConnectionState.NO_DEVICE_CONFIGURED;
+    }
+
+    private void notifyListeners() {
+        ConnectionState connectionState = determineConnectionState();
+        for (ConnectionStateListener listener : mListeners) {
+            listener.onConnectionStateChanged(connectionState);
+        }
+    }
 }

70e23329b2651626752f68e10433d0c4144d0bb9
Paul Mandal <paul.mandal@gmail.com>
2023-10-15 20:20:43 -0600

    Update to new ATAK API

--- app/src/main/java/com/paulmandal/atak/forwarder/plugin/ForwarderLifecycle.java
+++ app/src/main/java/com/paulmandal/atak/forwarder/plugin/ForwarderLifecycle.java
@@ -1,108 +1,12 @@
 package com.paulmandal.atak.forwarder.plugin;
 
-import android.app.Activity;
-import android.content.Context;
-import android.content.res.Configuration;
-
-import com.atakmap.android.maps.MapComponent;
-import com.atakmap.android.maps.MapView;
-import com.atakmap.coremap.log.Log;
-import com.paulmandal.atak.forwarder.BuildConfig;
-import com.paulmandal.atak.forwarder.ForwarderConstants;
+import com.atak.plugins.impl.AbstractPlugin;
 import com.paulmandal.atak.forwarder.plugin.ui.ForwarderMapComponent;
-import com.paulmandal.atak.libcotshrink.hackytests.HackyTests;
 
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.LinkedList;
+import gov.tak.api.plugin.IServiceController;
 
-import transapps.maps.plugin.lifecycle.Lifecycle;
-
-public class ForwarderLifecycle implements Lifecycle {
-    private final static String TAG = ForwarderConstants.DEBUG_TAG_PREFIX + ForwarderLifecycle.class.getSimpleName();
-
-    private final Context mPluginContext;
-    private MapView mMapView;
-    private final Collection<MapComponent> mOverlays;
-
-    public ForwarderLifecycle(Context context) {
-        mPluginContext = context;
-        mOverlays = new LinkedList<>();
-    }
-
-    @Override
-    public void onCreate(final Activity activity, final transapps.mapi.MapView transappsMapView) {
-        Log.d(TAG, "Starting ATAK Forwarder plugin");
-        if (transappsMapView == null || !(transappsMapView.getView() instanceof MapView)) {
-            Log.e(TAG, "This plugin is only compatible with ATAK MapView");
-            return;
-        }
-        mMapView = (MapView)transappsMapView.getView();
-        Context atakContext = mMapView.getContext();
-
-        if (BuildConfig.DEBUG) {
-            HackyTests hackyTests = new HackyTests();
-            hackyTests.runAllTests();
-        }
-
-        mOverlays.add(new ForwarderMapComponent());
-
-        // create components
-        Iterator<MapComponent> iter = mOverlays.iterator();
-        MapComponent c;
-        while (iter.hasNext()) {
-            c = iter.next();
-            try {
-                c.onCreate(mPluginContext, activity.getIntent(), mMapView);
-            } catch (Exception e) {
-                Log.w(TAG, "Unhandled exception trying to create overlays MapComponent", e);
-                iter.remove();
-            }
-        }
-    }
-
-    @Override
-    public void onDestroy() {
-        for (MapComponent c : mOverlays) {
-            c.onDestroy(mPluginContext, mMapView);
-        }
-    }
-
-    @Override
-    public void onStart() {
-        for (MapComponent c : mOverlays) {
-            c.onStart(mPluginContext, mMapView);
-        }
-    }
-
-    @Override
-    public void onPause() {
-        for (MapComponent c : mOverlays) {
-            c.onPause(mPluginContext, mMapView);
-        }
-    }
-
-    @Override
-    public void onResume() {
-        for (MapComponent c : mOverlays) {
-            c.onResume(mPluginContext, mMapView);
-        }
-    }
-
-    @Override
-    public void onFinish() {}
-
-    @Override
-    public void onStop() {
-        for (MapComponent c : mOverlays) {
-            c.onStop(mPluginContext, mMapView);
-        }
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration configuration) {
-        for (MapComponent c : mOverlays) {
-            c.onConfigurationChanged(configuration);
-        }
+public class ForwarderLifecycle extends AbstractPlugin {
+    public ForwarderLifecycle(IServiceController serviceController) {
+        super(serviceController, new ForwarderMapComponent());
     }
 }

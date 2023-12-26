dc50c896cd22059f23147dea5cd3ef2fef071a2e
Paul Mandal <paul.mandal@gmail.com>
2023-12-26 13:37:20 -0700

    Update to Meshtastic 2.2.16, disable plugin managing device by default

--- update-meshtastic-files.sh
+++ update-meshtastic-files.sh
@@ -8,5 +8,10 @@ sed -i "s/import com\.geeksville\.mesh\.util\.readParcelableCompat//" app/src/ma
 sed -i "s/\.readParcelableCompat/.readParcelable/g" app/src/main/java/com/geeksville/mesh/DataPacket.kt
 sed -i "s/import com\.geeksville\.mesh\.util\.anonymize//" app/src/main/java/com/geeksville/mesh/NodeInfo.kt
 sed -i "s/\.anonymize//g" app/src/main/java/com/geeksville/mesh/NodeInfo.kt
+sed -i "s/import androidx\.room\.Entity//" app/src/main/java/com/geeksville/mesh/MyNodeInfo.kt
+sed -i "s/import androidx\.room\.PrimaryKey//" app/src/main/java/com/geeksville/mesh/MyNodeInfo.kt
+sed -i "s/@Entity(tableName = \"MyNodeInfo\")//" app/src/main/java/com/geeksville/mesh/MyNodeInfo.kt
+sed -i "s/    @PrimaryKey(autoGenerate = false)//" app/src/main/java/com/geeksville/mesh/MyNodeInfo.kt
+
 
 cp -v ../Meshtastic-Android/app/src/main/aidl/com/geeksville/mesh/IMeshService.aidl app/src/main/aidl/com/geeksville/mesh/IMeshService.aidl
